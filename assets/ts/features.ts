87bdec6e10cf032ddc8fe6fee94225f1130625b2
Limbend <termir988@gmail.com>
2024-02-21 12:13:11 +0300

    fix: prettier formatting *.ts

--- assets/ts/features.ts
+++ assets/ts/features.ts
@@ -1,34 +1,35 @@
-import ThemeColorScheme from "ts/colorScheme"
-import { renderCopyButton } from "ts/copyButton"
-import { renderFootnotes } from "ts/footnotes"
+import ThemeColorScheme from "ts/colorScheme";
+import { renderCopyButton } from "ts/copyButton";
+import { renderFootnotes } from "ts/footnotes";
 
-let enableFootnotes = false
+let enableFootnotes = false;
 if (document.currentScript) {
-    enableFootnotes = document.currentScript.dataset.enableFootnotes == 'true'
+  enableFootnotes = document.currentScript.dataset.enableFootnotes == "true";
 }
 
 const init = () => {
-    new ThemeColorScheme(document.getElementById('dark-mode-button'))
-    if (enableFootnotes) {
-        renderFootnotes()
-    }
-    if (navigator && navigator.clipboard) {
-        renderCopyButton(navigator.clipboard);
-    } else {
-        var script = document.createElement('script');
-        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
-        script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
-        script.crossOrigin = 'anonymous';
-        script.onload = function() {
-            renderCopyButton(clipboard);
-        };
-    
-        document.body.appendChild(script);
-    }
-}
+  new ThemeColorScheme(document.getElementById("dark-mode-button"));
+  if (enableFootnotes) {
+    renderFootnotes();
+  }
+  if (navigator && navigator.clipboard) {
+    renderCopyButton(navigator.clipboard);
+  } else {
+    var script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
+    script.integrity = "sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=";
+    script.crossOrigin = "anonymous";
+    script.onload = function () {
+      renderCopyButton(clipboard);
+    };
 
-window.addEventListener('load', () => {
-    setTimeout(function () {
-        init()
-    }, 0)
-})
+    document.body.appendChild(script);
+  }
+};
+
+window.addEventListener("load", () => {
+  setTimeout(function () {
+    init();
+  }, 0);
+});
